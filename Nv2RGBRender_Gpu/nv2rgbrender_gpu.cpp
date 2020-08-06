14164222c5d1957f58442b59a32981f201b2ae23
wanghualin <18202879282@163.com>
2020-08-06 20:11:10 +0800

    debug nv2rgba

--- Nv2RGBRender_Gpu/nv2rgbrender_gpu.cpp
+++ Nv2RGBRender_Gpu/nv2rgbrender_gpu.cpp
@@ -2,7 +2,7 @@
  *
  * @Author: your name
  * @Date: 2020-08-02 11:10:34
- * @LastEditTime: 2020-08-06 10:30:43
+ * @LastEditTime: 2020-08-06 18:23:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vs_code\Nv12Render_Gpu\nv12render_gpu.cpp
@@ -12,7 +12,7 @@
 #include <Windows.h>
 #endif // WIN32
 #include <QMutex>
-#include <opencv2/opencv.hpp>
+#include <fstream>
 #include "ColorSpace.h"
 #include "nv2rgbrender_gpu.h"
 
@@ -70,7 +70,7 @@ void Nv2RGBRender_Gpu::initialize(const int width, const int height, const bool 
 
     QMutexLocker initLock(initMutex());
     const char *vsrc =
-        "attribute vec4 vertexIn; \
+		"attribute vec4 vertexIn; \
              attribute vec4 textureIn; \
              varying vec4 textureOut;  \
              void main(void)           \
@@ -169,7 +169,7 @@ void Nv2RGBRender_Gpu::initialize(const int width, const int height, const bool 
     glGenTextures(1, &texId);
 
     glBindTexture(GL_TEXTURE_2D, texId);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_BGRA, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, nullptr);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -178,13 +178,13 @@ void Nv2RGBRender_Gpu::initialize(const int width, const int height, const bool 
 
     glGenBuffers(1, &tex_bufferId);
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, tex_bufferId);
-    glBufferData(GL_PIXEL_UNPACK_BUFFER_ARB, width * height * 4 * sizeof(char), nullptr, GL_STREAM_DRAW_ARB);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER_ARB, width * height * 4, nullptr, GL_STREAM_DRAW_ARB);
 	glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, 0);
 
     glDisable(GL_DEPTH_TEST); //�򿪻��ڴ��ڴ�С�仯ʱ��������opengl������ʵ������Ҳ����Ҫ�򿪡�
 
     ck(cuCtxSetCurrent(context));
-    ck(cuGraphicsGLRegisterBuffer(&cuda_tex_resource, tex_bufferId, CU_GRAPHICS_REGISTER_FLAGS_READ_ONLY));
+    ck(cuGraphicsGLRegisterBuffer(&cuda_tex_resource, tex_bufferId, CU_GRAPHICS_REGISTER_FLAGS_WRITE_DISCARD));
 	ck(cuMemAllocHost(&file_ptr, (size_t)width*height * 4));
 }
 
@@ -198,16 +198,21 @@ void Nv2RGBRender_Gpu::render(unsigned char *nv12_dPtr, const int width, const i
     }
 
     ck(cuCtxSetCurrent(context));
+
     CUdeviceptr d_tex_buffer;
     size_t d_tex_size;
     ck(cuGraphicsMapResources(1, &cuda_tex_resource, 0));
     ck(cuGraphicsResourceGetMappedPointer(&d_tex_buffer, &d_tex_size, cuda_tex_resource));
-    Nv12ToColor32<BGRA32>(reinterpret_cast<uint8_t *>(nv12_dPtr), width, (uint8_t *)d_tex_buffer, width, width, height);
+	uint8_t *hostp;
+	cudaMalloc(&hostp, d_tex_size);
+    Nv12ToColor32<BGRA32>(reinterpret_cast<uint8_t *>(nv12_dPtr), width, hostp, d_tex_size / height, width, height, 5);
 	static int index = 0;
 	qDebug() << "----------" << index << "src pitch" << d_tex_size / (4 * height) << "d_tex_size" << d_tex_size;
-	ck(cudaMemcpy(file_ptr, reinterpret_cast<void*>(d_tex_buffer), width*height * 4 * sizeof(char), cudaMemcpyDeviceToHost));
-	std::ofstream ofile( "images/" + std::to_string(index++), std::ios::binary);
+//	ck(cudaMemcpy(file_ptr, reinterpret_cast<void*>(hostp), width* height * 4, cudaMemcpyDeviceToHost));
+	ck(cudaMemcpy(file_ptr, reinterpret_cast<void*>(nv12_dPtr), width* height * 3 / 2, cudaMemcpyDeviceToHost));
+	std::ofstream ofile( "images/" + std::to_string(index), std::ios::binary);
 	ofile.write(reinterpret_cast<char*>(file_ptr), width*height * 4);
+	index++;
     ck(cuGraphicsUnmapResources(1, &cuda_tex_resource, 0));
     ck(cuCtxSetCurrent(nullptr));
 
@@ -218,11 +223,10 @@ void Nv2RGBRender_Gpu::render(unsigned char *nv12_dPtr, const int width, const i
     program.setAttributeBuffer("vertexIn", GL_FLOAT, 0, 2, 2 * sizeof(GLfloat));
     program.setAttributeBuffer("textureIn", GL_FLOAT, 2 * 4 * sizeof(GLfloat), 2, 2 * sizeof(GLfloat));
 
-    //glActiveTexture(GL_TEXTURE0 + 0);
+    glActiveTexture(GL_TEXTURE0 + 0);
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, tex_bufferId);
     glBindTexture(GL_TEXTURE_2D, texId);
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, nullptr);
-	glBindTexture(GL_TEXTURE_2D, 0);
 	glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, 0);
 
     program.setUniformValue("uTexture", 0);
